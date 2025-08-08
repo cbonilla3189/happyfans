bce29567aed2e1f37c51881e9fb6834ee5f617f8
cbonilla3189 <147958786+cbonilla3189@users.noreply.github.com>
2025-08-08 12:57:37 -0500

    Create init_db.py

---
+++ init_db.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Fan(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    message = db.Column(db.String(200), nullable=False)
+
+with app.app_context():
+    db.create_all()
+    print("Base de datos inicializada y tabla 'Fan' creada correctamente.")
