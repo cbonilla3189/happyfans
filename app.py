121a4411eb9e91bf7420461a16e6711c763c928c
cbonilla3189 <147958786+cbonilla3189@users.noreply.github.com>
2025-08-08 12:56:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
 
-app = Flask (__name__)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Fan(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    message = db.Column(db.String(200), nullable=False)
 
 @app.route("/")
 def home():
-    return "¡Hola, Carlos! Esta es tu app Flask desplegada en Render."
+    return "¡Hola, Carlos! Happy Fans está conectado a la base de datos."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
