6dae3087e70230fb09e16b7b1799a1f1cb6db939
cbonilla3189 <147958786+cbonilla3189@users.noreply.github.com>
2025-08-08 13:36:18 -0500

    Update app.py

--- app.py
+++ app.py
@@ -66,4 +66,4 @@ def uploaded_file(filename):
 
 # Punto de entrada
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
